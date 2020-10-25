f0b24e4b688c8cc621951bfd09ca3a2ef561282b
Antonio Carlos Furtado <marcojrfurtado@gmail.com>
2020-10-24 23:29:16 -0400

    Bump to 0.0.5

--- setup.py
+++ setup.py
@@ -91,6 +91,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/marcojrfurtado/osmosis-streaming-driver',
-    version='0.0.4',
+    version='0.0.5',
     zip_safe=False,
 )
